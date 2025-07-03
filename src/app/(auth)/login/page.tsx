ded56377b959469c7dd5cd463255c10a576ec6b7
Hacker Gaming <hg7095947@gmail.com>
2025-07-03 06:35:41 +0000

    created register page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,79 @@
+"use client"
+import { useState } from "react";
+import Link from "next/link";
+
+const Login = () => {
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const handleSubmit = async (e:React.FormEvent) => {
+    e.preventDefault()
+    console.log(formData)
+    // TODO: Add login logic
+    setFormData ({
+      email: "",
+      password: ""
+    })
+  }
+  
+  return (
+    <div className="mt-8 md:mt-20">
+      <h1 className="text-center font-semibold text-xl md:text-5xl">Welcome back to Apna Hisab</h1>
+      <div className="form-container mt-8 px-4 border border-zinc-300 m-4 rounded-lg shadow-md shadow-zinc-300 py-8">
+        <h2 className="text-center font-semibold text-lg md:text-4xl">Login to your account</h2>
+        <form 
+          onSubmit={handleSubmit}
+          className="mt-8 mx-auto md:mx-16">
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 focus:outline-blue-500 md:text-2xl md:font-semibold md:mb-4 md:mt-8"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-blue-500 md:h-18 md:text-2xl md:font-semibold"
+              placeholder="name@email.com"
+              value={formData.email}
+              onChange={(e)=>setFormData({...formData, email: e.target.value})}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900  focus:outline-blue-500 md:text-2xl md:font-semibold md:mb-4 md:mt-8"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-blue-500 md:h-18 md:text-2xl md:font-semibold"
+              placeholder="Your password"
+              value={formData.password}
+              onChange={(e)=>setFormData({...formData, password: e.target.value})}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center md:text-2xl md:font-semibold md:mt-8"
+          >
+            Login
+          </button>
+        </form>
+        <p className="mt-4 text-center text-sm font-medium text-grey-900 md:text-2xl md:font-semibold md:mb-8">
+          Don't have account?{" "} <Link href="/register" className="text-blue-500 hover:underline">Create New</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,18 @@ import { IoLogInOutline } from "react-icons/io5";
 const Header = () => {
   return (
     <div>
-      <nav className="bg-zinc-700 text-white px-2 h-14 flex justify-between items-center">
+      <nav className="bg-zinc-700 text-white px-2 h-14 flex justify-between items-center md:h-24 md:px-8">
         <div>
           <Link href="/">
-            <h1 className="text-lg font-semibold">Expense Tracker</h1>
+            <h1 className="text-lg font-semibold md:text-4xl">Expense Tracker</h1>
           </Link>
         </div>
         <div>
           <Link 
-            className="flex items-center gap-1 text-lg"
+            className="flex items-center gap-1 text-lg md:text-3xl md:font-semibold"
             href="/login">
             <IoLogInOutline
-              className="text-2xl"
+              className="text-2xl md:text-4xl"
               />
             Login
           </Link>
